e7bc022cdb966db0dbdac1d8fb4f0383fa8549f8
losfair <zhy20000919@hotmail.com>
2016-12-10 09:35:08 +0800

    Add build scripts

---
+++ new_frontend/build.bat
@@ -0,0 +1,1 @@
+electron-packager src CloudEdu --platform=win32 --arch=all --version=1.4.11 --out=Staging --version-string.ProductName=CloudEdu --version-string.ProductVersion=0.1.0
---
+++ new_frontend/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+electron-packager src CloudEdu --platform=win32 --arch=all --version=1.4.11 --out=Staging --version-string.ProductName=CloudEdu --version-string.ProductVersion=0.1.0
